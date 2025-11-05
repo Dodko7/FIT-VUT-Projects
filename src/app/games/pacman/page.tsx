3b69afbe17d20fbe31f824d36294857d0d102aa7
robertpales <pales.robo@gmail.com>
2025-11-05 16:09:13 +0100

    feat: add back button, add text with animation after win and lose,add reset logic, update structure

--- src/app/games/pacman/page.tsx
+++ src/app/games/pacman/page.tsx
@@ -1,9 +1,13 @@
-import PacmanGame from '../../../components/games/pacman/pacman';
+import PacmanGame from "../../../components/games/pacman/pacman";
+import Link from "next/link";
 
 export default function PacmanGamePage() {
-  return (
-    <div className="min-h-screen bg-black">
-      <PacmanGame />
-    </div>
-  );
+	return (
+		<div className="relative min-h-screen bg-black">
+			<Link href="/">
+				<button className="absolute top-10 left-10 z-2 m-5 cursor-pointer bg-yellow-400 px-5 py-2 font-['Press_Start_2P'] text-white">Back</button>
+			</Link>
+			<PacmanGame />
+		</div>
+	);
 }
